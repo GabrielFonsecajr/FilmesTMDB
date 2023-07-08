62a8e18d22f3d834d9ec39bc2b19f3f24d1d14cf
Gabriel Fonseca <54801529+GabrielFonsecajr@users.noreply.github.com>
2023-07-07 22:21:35 -0300

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,93 @@
+import { View, Text, TouchableOpacity, ScrollView, Platform } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import TrendingMovies from '../components/trendingMovies';
+import MovieList from '../components/movieList';
+import { StatusBar } from 'expo-status-bar';
+import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb';
+import { useNavigation } from '@react-navigation/native';
+import Loading from '../components/loading';
+import { styles } from '../theme';
+
+const ios = Platform.OS === 'ios';
+
+export default function HomeScreen() {
+
+  const [trending, setTrending] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+
+
+  useEffect(()=>{
+    getTrendingMovies();
+    getUpcomingMovies();
+    getTopRatedMovies();
+  },[]);
+
+  const getTrendingMovies = async ()=>{
+    const data = await fetchTrendingMovies();
+    console.log('Tendência', data.results.length)
+    if(data && data.results) setTrending(data.results);
+    setLoading(false)
+  }
+  const getUpcomingMovies = async ()=>{
+    const data = await fetchUpcomingMovies();
+    console.log('Por vir', data.results.length)
+    if(data && data.results) setUpcoming(data.results);
+  }
+  const getTopRatedMovies = async ()=>{
+    const data = await fetchTopRatedMovies();
+    console.log('Melhores Avaliados', data.results.length)
+    if(data && data.results) setTopRated(data.results);
+  }
+
+
+
+  return (
+    <View className="flex-1 bg-red-950">
+      {/* search bar */}
+      <SafeAreaView className={ios? "-mb-2": "mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="red" />
+          <Text 
+            className="text-white text-3xl font-bold">
+              <Text style={styles.text}>F</Text>ilmes
+          </Text>
+          <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="red" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      {
+        loading? (
+          <Loading />
+        ):(
+          <ScrollView 
+            showsVerticalScrollIndicator={false} 
+            contentContainerStyle={{paddingBottom: 10}}
+          >
+
+            {/* Trending Movies Carousel */}
+            { trending.length>0 && <TrendingMovies title="Tendência" data={trending} /> }
+
+            {/* upcoming movies row */}
+            { upcoming.length>0 && <MovieList title="Por vir" data={upcoming} /> }
+            
+
+            {/* top rated movies row */}
+            { topRated.length>0 && <MovieList title="Melhores Filmes" data={topRated} /> }
+
+          </ScrollView>
+        )
+      }
+      
+  </View>
+      
+
+   
+  )
+}
